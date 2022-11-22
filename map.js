777164983065fed98ca2e0551ee4d6ab64b2e75c
araldwin <araldwin@gmail.com>
2022-11-22 06:41:08 +0000

    Map method

--- map.js
+++ map.js
@@ -5,15 +5,31 @@
  */
 
 // Using a for loop
+let nums =[1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
 
+const simplified = nums.map(function (num) { return num * 2});
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 
 // With objects:
@@ -37,3 +53,47 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+// Advanced JavaScript - The map method Challenge
+
+let students1 = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, maths: 77, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+// single line, used different variable names so as not to clash with those below.
+let [John, ...Rest] = students1.map(itm => [itm.name, itm.results]);
+console.log(John);
+console.log(Rest);
+
+console.log('\n');
+// Or create function first and use in map
+const mapper = (itm) => [itm.name, itm.results];
+let [john, ...rest] = students1.map(mapper);
+console.log(john);
+console.log(rest);
